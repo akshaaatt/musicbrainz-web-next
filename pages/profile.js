e45a973c2a56766ffde14921bf6f9dd68aa166d9
amoghjalan <amoghjalan2005@gmail.com>
2022-01-14 11:49:01 +0530

    Fixes #8
    Signed-off-by: amoghjalan <amoghjalan2005@gmail.com>

--- pages/profile.js
+++ pages/profile.js
@@ -1,34 +1,42 @@
-import React, {useCallback, useState} from "react";
+import React, { useCallback, useState, useEffect } from "react";
 import Header from "../components/Shared/Header/Header";
 import Footer from "../components/Shared/Footer/Footer";
 import ScrollToTop from "../components/Utils/ScrollToTop/ScrollToTop";
 import MainProfile from "../components/Profile/MainProfile";
-import {themeSetting} from "../components/Utils/Common";
+import { themeSetting } from "../components/Utils/Common";
 
 export default function ProfilePage() {
-    const [ dark, setDark ] = useState(themeSetting.setting);
-    let theme = dark ? "theme-dark" : "theme-light";
+  const [dark, setDark] = useState(themeSetting.setting);
+  let theme = dark ? "theme-dark" : "theme-light";
 
-    const toggleDarkMode = useCallback(function () {
-        setDark(prevState => {
-            const newState = !prevState;
-            themeSetting.setting = prevState;
-            return newState;
-        });
-    }, []);
+  const toggleDarkMode = useCallback(function () {
+    setDark(prevState => {
+      const newState = !prevState;
+      themeSetting.setting = newState;
+      return newState;
+    });
+  }, []);
 
-    return (
-        <div>
-            <Header theme={theme} switchActiveTheme={toggleDarkMode} isDarkThemeActive={dark}/>
-            <MainProfile theme={theme} switchActiveTheme={toggleDarkMode} isDarkThemeActive={dark}/>
-            <Footer theme={theme}/>
-            <ScrollToTop
-                icon="bi bi-caret-up-fill"
-                backgroundColor = "#EB743B"
-                position={{ bottom: "12%", right: "0%" }}
-                hover={{ backgroundColor: "purple", opacity: "0.95" }}
-                margin="24px"
-            />
-        </div>
-    );
+  return (
+    <div>
+      <Header
+        theme={theme}
+        switchActiveTheme={toggleDarkMode}
+        isDarkThemeActive={dark}
+      />
+      <MainProfile
+        theme={theme}
+        switchActiveTheme={toggleDarkMode}
+        isDarkThemeActive={dark}
+      />
+      <Footer theme={theme} />
+      <ScrollToTop
+        icon="bi bi-caret-up-fill"
+        backgroundColor="#EB743B"
+        position={{ bottom: "12%", right: "0%" }}
+        hover={{ backgroundColor: "purple", opacity: "0.95" }}
+        margin="24px"
+      />
+    </div>
+  );
 }
