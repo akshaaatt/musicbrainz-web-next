f3a82059f54053ee33d1d7fef830a83d391d020e
asubedy <frexpe@pm.me>
2022-01-15 22:48:58 +0545

    remove axios and use fetch
    Signed-off-by: asubedy <frexpe@pm.me>

--- components/Home/Intro/Blog.js
+++ components/Home/Intro/Blog.js
@@ -3,28 +3,31 @@ import Carousel from "react-multi-carousel";
 import dynamic from 'next/dynamic';
 import {Modal} from "react-bootstrap";
 import Image from 'next/image';
-import axios from "axios";
+
+
 
 
 const Blog = () => {
     const [blogDetails,setBlogDetails] = useState([{}])
-  useEffect(()=>{
-    axios
-      .get(`https://public-api.wordpress.com/rest/v1.1/sites/blog.metabrainz.org/posts/`)
-      .then(response=>{
-        const objectArray =[]
+
+    const fetchBlogDetails = async () =>{
+      let response = await fetch(`https://public-api.wordpress.com/rest/v1.1/sites/blog.metabrainz.org/posts/`)
+      response = await response.json()
+      const objectArray =[]
         for(let postIndex = 0;postIndex<=7; postIndex++ ){
           const object = {};
-          object.id=response.data.posts[postIndex].ID
-          object.title=response.data.posts[postIndex].title
-          object.link=response.data.posts[postIndex].URL
+          object.id=response.posts[postIndex].ID
+          object.title=response.posts[postIndex].title
+          object.link=response.posts[postIndex].URL
           objectArray.push(object)
         }
         setBlogDetails(objectArray)
-      })
+    }
+  useEffect(()=>{
+    fetchBlogDetails()
   },[])
 
-    return(
+  return(
         <div className="card">
     <Image className="card-img-top" src="/assets/img/blogs.svg" width="128" height="48" alt="Blogs Logo"/>
     <div className="card-body">
@@ -34,6 +37,7 @@ const Blog = () => {
         {blogDetails.map(post =>
             <li key={post.id} className={"list-group-item"}><a href={post.link} target="_blank" rel="noopener noreferrer" className="card-link">{post.title}</a></li> 
         )}
+        
     </ul>
     <div className="card-body align-items-center d-flex justify-content-center">
         <a href="https://twitter.com/MusicBrainz" target="_blank" rel="noopener noreferrer" className="card-link"> <i className="fab fa-twitter"/></a>
