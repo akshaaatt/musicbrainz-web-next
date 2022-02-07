03551ecba6d41a841f20346ab59094040f5a5d40
asubedy <frexpe@pm.me>
2022-02-07 23:55:26 +0545

    changed advance-search icon
    Signed-off-by: asubedy <frexpe@pm.me>

--- components/Shared/Header.js
+++ components/Shared/Header.js
@@ -54,6 +54,11 @@ export default function Header(props) {
                 <li className="nav-item">
                   <a href="https://blog.metabrainz.org" target="_blank" rel="noopener noreferrer" className="nav-link " >Community</a>
                 </li>
+                <Link href="/search">
+                <li className='nav-item'>
+                <a className="nav-link " >Search</a>
+                </li>
+                </Link>
 
                 <li className="nav-item dropdown">
                   <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Username</a>
@@ -129,7 +134,7 @@ export default function Header(props) {
               </select>
             </div>
             <button  id='search_button' type="button" onClick={attach} className="btn btn-b-n">
-                   Advance Search
+                <Image  alt= "search_logo" src="/assets/img/advance_search.svg" width="22px" height="22px"/>
             </button>
 
           </div>
@@ -165,12 +170,10 @@ const changeIcon = () =>{
   const query = document.getElementById('searchInputHeader');
   let search_button = document.getElementById('search_button')
   if(query.value.length <= 1){
-    search_button.innerHTML = "Advance Search"
+    search_button.innerHTML = `<Image alt= "search_logo" src="/assets/img/advance_search.svg" width="20px" height="20px" />`
   }
   else{
-    search_button.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
-    <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
-    </svg>`
+    search_button.innerHTML = `<Image alt= "search_logo" src="/assets/img/search.svg" width="20px" height="20px" />`
   }
   
 }
