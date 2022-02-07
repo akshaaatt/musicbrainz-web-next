f7ad8e7071c96e456cafc34603282a578e0a3e0f
asubedy <frexpe@pm.me>
2022-02-07 11:18:58 +0545

    changed the behaviour of header search button
    Signed-off-by: asubedy <frexpe@pm.me>

--- components/Shared/Header.js
+++ components/Shared/Header.js
@@ -101,6 +101,9 @@ export default function Header(props) {
                          return false;
                        }
                      }}
+                     onChange={event =>{
+                       changeIcon();
+                     }}
                      placeholder="Search"/>
             </div>
 
@@ -125,8 +128,8 @@ export default function Header(props) {
 
               </select>
             </div>
-            <button type="button" onClick={attach} className="btn btn-b-n">
-              <i className="bi bi-search"/>
+            <button  id='search_button' type="button" onClick={attach} className="btn btn-b-n">
+                   Advance Search
             </button>
 
           </div>
@@ -158,3 +161,16 @@ const attach = e => {
   document.body.classList.remove('box-collapse-closed')
 }
 
+const changeIcon = () =>{
+  const query = document.getElementById('searchInputHeader');
+  let search_button = document.getElementById('search_button')
+  if(query.value.length <= 1){
+    search_button.innerHTML = "Advance Search"
+  }
+  else{
+    search_button.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
+    <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+    </svg>`
+  }
+  
+}
