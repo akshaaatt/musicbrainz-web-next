3f379a92dceac94001ad1d725899f4c54246d5e6
asubedy <frexpe@pm.me>
2022-02-12 01:10:42 +0545

    added useeffect functions to fetch search details
    Signed-off-by: asubedy <frexpe@pm.me>

--- components/Search/Results.js
+++ components/Search/Results.js
@@ -1,7 +1,36 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 export default function Results(props){
     const [queryDetails,setQueryDetails] = useState([{}])
-
+    
+    const fetchSearchDetails = async () =>{
+        let response = await fetch(`https://musicbrainz.org/ws/2/artist/?fmt=json&query=hello&offset=0`)
+        response = await response.json()
+        const objectArray =[]
+        console.log(response.artists[1]);
+        for(let detailsIndex = 0;detailsIndex<response.artists.length;detailsIndex++){
+            const object = {};
+            object.id =response.artists[detailsIndex].id
+            object.name = response.artists[detailsIndex].name 
+            object.sortName = response.artists[detailsIndex]['sort-name']
+            object.disambiguation = response.artists[detailsIndex].disambiguation ? `( ${response.artists[detailsIndex].disambiguation})`: null 
+            object.type = response.artists[detailsIndex].type ? response.artists[detailsIndex].type : null ;
+            object.area = response.artists[detailsIndex].area ? response.artists[detailsIndex].area.name : null ;
+            object.beginArea = response.artists[detailsIndex]['begin-area'] ? response.artists[detailsIndex]['begin-area'].name : null;
+            object.begin = response.artists[detailsIndex]['life-span'] ? response.artists[detailsIndex]['life-span'].begin : null;
+            object.end = response.artists[detailsIndex]['life-span'] ? response.artists[detailsIndex]['life-span'].ended : null;
+            object.endArea = response.artists[detailsIndex].area? response.artists[detailsIndex].area['life-span'].ended : null;
+            
+  
+            objectArray.push(object)
+        }
+          console.log(objectArray); 
+        
+        setQueryDetails(objectArray)
+      }
+    
+    useEffect(()=>{
+        fetchSearchDetails()
+    },[])
     return(
         <div className={"result_table "+ props.theme}>
             <table className="table table-striped">
